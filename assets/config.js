9530f8d0131b3d6c9f6ac82d45483da18e87f84b
Trustin326 <trustin326@icloud.com>
2026-05-23 17:09:49 -0400

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: "YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL",
+  API_URL: "https://script.google.com/macros/s/AKfycbzDPxF4ZiJsnS86wZ8b-U2QRichvQukoNHqxLS37nQrhW0b8-ItntiVT05i6OTLVFoa/exec",
   STRIPE_LINK: "YOUR_STRIPE_CHECKOUT_LINK",
   ARTIST_LISTING_STRIPE_LINK: "YOUR_ARTIST_LISTING_STRIPE_LINK",
   OWNER_EMAIL: "owner@lashacademy.com",
